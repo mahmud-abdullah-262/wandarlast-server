5180f8b8d376a4a5e252e0a296649a1d34c150f1
mahmud-abdullah-262 <abdullah624683@gmail.com>
2026-05-11 21:35:05 +0600

    server done with crud oparation

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const port = process.env.PORT || 5000
 const cors = require('cors');
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB_URI;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = process.env.MONGODB_URI 
+console.log(uri, 'uri')
 
 
 app.use(cors())
@@ -31,14 +32,72 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db('wandarlast-db')
+    const destinationCollection = db.collection('destinations');
+    
+
+    app.post('/destination', async (req, res) => {
+      const destinationData = req.body;
+      const result = await destinationCollection.insertOne(destinationData)
+      console.log(result, 'data on server')
+      res.send(result)
+
+    })
+
+    app.get('/destination', async (req, res) => {
+     const cursor = destinationCollection.find() 
+      const result = await cursor.toArray()
+      res.send(result) 
+    })
+
+     app.get('/destination/:id', async (req, res) => {
+     const {id} = req.params;
+      const result = await destinationCollection.findOne({_id: new ObjectId(id)})
+      res.send(result) 
+    })
+
+
+    app.patch('/destination/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id, 'id')
+      const query = {_id: new ObjectId(id)}
+      const modifiedDestination = req.body;
+      const updatedDocument = {
+          $set : {
+            destinationName: modifiedDestination.destinationName,
+            country: modifiedDestination.country,
+            category: modifiedDestination.category,
+            price: modifiedDestination.price,
+            duration: modifiedDestination.duration,
+            departureDate: modifiedDestination.departureDate,
+            imageUrl: modifiedDestination.imageUrl,
+            description: modifiedDestination.description,
+          
+          }
+      }
+      const result = await destinationCollection.updateOne(query, updatedDocument);
+      console.log(result, 'modified destination')
+      res.send(result)
+    })
+
+       app.delete('/destination/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id) }
+      const result = await destinationCollection.deleteOne(query)
+      res.json(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
